bfe3e73bdc1867aeb81ea017ca2ce4aaa34d7ce8
vidy <vidy3587@gmail.com>
2020-01-09 10:48:54 +0800

    change download url

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # json2ts
 a jetbrain plugin convert json to typescript
 
-[download url](https://github.com/boneVidy/json2ts/releases/download/1.0.2/com.jsonts.kotlin-1.0.2-SNAPSHOT.zip)
+[download url](https://github.com/boneVidy/json2ts/suites/391205561/artifacts/1021684)
--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -4,7 +4,11 @@ import com.google.gson.*
 import exceptions.SyntaxException
 
 
-fun toTypescript(jsonString: String, objectName: String = "RootObject", parseType: ParseType = ParseType.InterfaceStruct): String {
+fun toTypescript(
+    jsonString: String,
+    objectName: String = "RootObject",
+    parseType: ParseType = ParseType.InterfaceStruct
+): String {
     val jsonContent = try {
         parserFromJsonString(jsonString)
     } catch (e: Exception) {
@@ -14,24 +18,18 @@ fun toTypescript(jsonString: String, objectName: String = "RootObject", parseTyp
         return "null"
     }
     if (jsonContent.isJsonArray) {
-        return toTsStruct(jsonContent.asJsonArray[0], objectName, parseType )
+        return toTsStruct(jsonContent.asJsonArray[0], objectName, parseType)
     }
     return toTsStruct(jsonContent, objectName, parseType)
 }
 
-private fun parserFromJsonString(src: String): JsonElement? {
-    return JsonParser().parse(src).asJsonObject
-}
+private fun parserFromJsonString(src: String): JsonElement? = JsonParser().parse(src).asJsonObject
 
+private fun upcaseFirstChar(key: String): String = key[0].toUpperCase() + key.substring(1, key.length)
 
+private fun lowcaseFirstChar(key: String): String = key[0].toLowerCase() + key.substring(1, key.length)
 
-private fun upcateFirstChar(key: String): String {
-    return key[0].toUpperCase() + key.substring(1, key.length)
-}
-private fun lowcaseFirstChar(key: String): String {
-    return key[0].toLowerCase() + key.substring(1, key.length)
-}
-private fun toTsStruct(jsonElement: JsonElement, objectName: String = "RootObject", parseType: ParseType ):String {
+private fun toTsStruct(jsonElement: JsonElement, objectName: String = "RootObject", parseType: ParseType): String {
     val optionalKeys = mutableListOf<String>()
     val objectResult = mutableListOf<String>()
     val json = jsonElement.asJsonObject
@@ -42,25 +40,25 @@ private fun toTsStruct(jsonElement: JsonElement, objectName: String = "RootObjec
         val key = o.key
         if (value.isJsonObject && !value.isJsonArray) {
 
-            val childObjectName = upcateFirstChar(key)
+            val childObjectName = upcaseFirstChar(key)
             objectResult.add(toTsStruct(value, childObjectName, parseType))
             json.addProperty(key, "$childObjectName;")
         } else if (value.isJsonArray) {
             val valueArr = value.asJsonArray
             val arrayTypes = detectMultiArrayTypes(value)
             if (arrayTypes.size > 1) {
-                val multiArrayBrackets= getMultiArrayBrackets(value.toString())
+                val multiArrayBrackets = getMultiArrayBrackets(value.toString())
                 if (isAllEquals(arrayTypes)) {
-                    json.addProperty(key, arrayTypes[0].replace("[]",multiArrayBrackets))
+                    json.addProperty(key, arrayTypes[0].replace("[]", multiArrayBrackets))
 
                 } else {
-                    json.addProperty(key, "any"+multiArrayBrackets+";")
+                    json.addProperty(key, "any" + multiArrayBrackets + ";")
                 }
 
             } else if (valueArr.size() > 0 && valueArr[0].isJsonObject) {
-                val childObjectName = upcateFirstChar(key)
-                objectResult.add(toTsStruct(valueArr[0],childObjectName, parseType))
-                json.addProperty(key,removeMajority(childObjectName)+"[];")
+                val childObjectName = upcaseFirstChar(key)
+                objectResult.add(toTsStruct(valueArr[0], childObjectName, parseType))
+                json.addProperty(key, removeMajority(childObjectName) + "[];")
             } else {
                 json.addProperty(key, arrayTypes[0])
             }
@@ -82,10 +80,12 @@ private fun toTsStruct(jsonElement: JsonElement, objectName: String = "RootObjec
     objectResult.add(result)
     return objectResult.joinToString("\n\n")
 }
-private fun toJsonEle (src: String):JsonElement {
+
+private fun toJsonEle(src: String): JsonElement {
     return JsonParser().parse(src)
 }
-private fun isAllEquals (array: List<String>):Boolean {
+
+private fun isAllEquals(array: List<String>): Boolean {
     val fist = array[0];
     val list = array.slice(1..array.size)
     for (item in list) {
@@ -95,6 +95,7 @@ private fun isAllEquals (array: List<String>):Boolean {
     }
     return true
 }
+
 private fun getMultiArrayBrackets(content: String): String {
     var brackets = "";
     content.forEach { char ->
@@ -106,39 +107,41 @@ private fun getMultiArrayBrackets(content: String): String {
     }
     return brackets
 }
-private fun checkIsString (item: JsonElement):Boolean {
-    return item.isJsonPrimitive && item.asJsonPrimitive.isString
-}
-private fun checkIsNumber (item: JsonElement):Boolean {
-    return item.isJsonPrimitive && item.asJsonPrimitive.isNumber
-}
-private fun checkIsBoolean (item: JsonElement):Boolean {
-    return item.isJsonPrimitive && item.asJsonPrimitive.isBoolean
-}
-private fun checkJsonArrayType (jsonArray: JsonArray, checkFn: (item:JsonElement) -> Boolean):Boolean {
+
+private fun checkIsString(item: JsonElement): Boolean = item.isJsonPrimitive && item.asJsonPrimitive.isString
+
+private fun checkIsNumber(item: JsonElement): Boolean = item.isJsonPrimitive && item.asJsonPrimitive.isNumber
+
+private fun checkIsBoolean(item: JsonElement): Boolean = item.isJsonPrimitive && item.asJsonPrimitive.isBoolean
+
+private fun checkJsonArrayType(jsonArray: JsonArray, checkFn: (item: JsonElement) -> Boolean): Boolean {
     var ret = true
     jsonArray.forEach { item -> ret = checkFn(item) }
     return ret
 }
-private fun detectMultiArrayTypes(value: JsonElement, valueType: MutableList<String> = mutableListOf<String>()): MutableList<String> {
+
+private fun detectMultiArrayTypes(
+    value: JsonElement,
+    valueType: MutableList<String> = mutableListOf<String>()
+): MutableList<String> {
     val list = mutableListOf<String>()
-    list.addAll( valueType)
+    list.addAll(valueType)
     if (value.isJsonArray) {
         val arr = value.asJsonArray;
         if (arr.size() == 0) {
             list.add("any[];")
-        } else if(arr[0].isJsonArray) {
+        } else if (arr[0].isJsonArray) {
             for (item in arr) {
-               val  valueTypeResult = detectMultiArrayTypes(item, valueType)
+                val valueTypeResult = detectMultiArrayTypes(item, valueType)
                 list.addAll(valueTypeResult)
             }
         } else if (checkJsonArrayType(arr, ::checkIsString)) {
             list.add("string[];")
         } else if (checkJsonArrayType(arr, ::checkIsNumber)) {
             list.add("number[];")
-        } else if (checkJsonArrayType(arr,::checkIsBoolean)) {
+        } else if (checkJsonArrayType(arr, ::checkIsBoolean)) {
             list.add("boolean[]")
-        } else{
+        } else {
             list.add("any[]")
         }
 
@@ -147,28 +150,32 @@ private fun detectMultiArrayTypes(value: JsonElement, valueType: MutableList<Str
 }
 
 
-private fun removeMajority(objectName: String): String{
+private fun removeMajority(objectName: String): String {
     val set = setOf<RegexOption>(RegexOption.IGNORE_CASE);
     val reg = Regex("IES$", set)
     val len = objectName.length
     if (reg.matches(objectName)) {
         return objectName.substring(0, len - 3) + "y"
     } else if (objectName.contains("S")) {
-        return objectName.substring(0, len -1)
+        return objectName.substring(0, len - 1)
     }
     return objectName
 }
-private fun formatCharsToTypeScript(jsonContent: JsonObject, objectName: String, optionalKeys: List<String>, parseType: ParseType): String {
+
+private fun formatCharsToTypeScript(
+    jsonContent: JsonObject,
+    objectName: String,
+    optionalKeys: List<String>,
+    parseType: ParseType
+): String {
     val reg = Regex("\"")
     var result = Gson()
         .newBuilder()
         .setPrettyPrinting().create()
         .toJson(jsonContent)
         .replace(reg, "")
-        .replace(Regex(","),"")
-
+        .replace(Regex(","), "")
     val entrySets = jsonContent.entrySet()
-
     for (item in entrySets) {
         val key = item.key
         if (optionalKeys.contains(key)) {
