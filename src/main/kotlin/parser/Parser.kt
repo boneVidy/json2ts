4428a9e1f2c65d1b2f84bc645cba5ffaaff7ec4b
vidy <vidy3587@gmail.com>
2020-07-13 17:51:41 +0800

    fix null pointer bug when input a empty array

--- src/main/kotlin/parser/Parser.kt
+++ src/main/kotlin/parser/Parser.kt
@@ -13,10 +13,14 @@ fun toTypescript(
         return e.message ?: "parser error";
     }
     if (jsonElement.isJsonArray) {
-        val firstEle = jsonElement.asJsonArray[0];
+        val jsonArray = jsonElement.asJsonArray;
+        if (jsonArray.size() == 0) {
+            return "export type $objectName = any[];";
+        }
+        val firstEle = jsonArray[0]
         if (firstEle.isJsonPrimitive) {
             val primitive = firstEle.asJsonPrimitive;
-            return toPrimitiveArray(primitive, objectName)
+            return toPrimitiveArray(objectName, primitive)
         }
         val typeStr = toTsStruct(jsonElement.asJsonArray[0], parseType, "RootDataItem");
         return """$typeStr; 
@@ -26,17 +30,22 @@ export type ${objectName}s = RootDataItem[];
     return toTsStruct(jsonElement, parseType, objectName)
 }
 
-private fun toPrimitiveArray (jsonPrimitive: JsonPrimitive,  name: String):String = when {
-    jsonPrimitive.isBoolean -> {
-        "export type $name = boolean[];";
+private fun toPrimitiveArray(name: String, jsonPrimitive: JsonPrimitive?): String = when (jsonPrimitive) {
+    null -> {
+        "export type $name = any[];";
     }
-    jsonPrimitive.isString -> {
-        "export type $name = string[];";
+    else -> when {
+        jsonPrimitive.isBoolean -> {
+            "export type $name = boolean[];";
+        }
+        jsonPrimitive.isString -> {
+            "export type $name = string[];";
+        }
+        jsonPrimitive.isNumber -> {
+            "export type $name = number[];";
+        }
+        else -> "export type $name = any[];";
     }
-    jsonPrimitive.isNumber -> {
-        "export type $name = number[];";
-    }
-    else -> "export type $name = any[];";
 }
 
 private fun uppercaseFirstChar(key: String): String = key[0].toUpperCase() + key.substring(1, key.length)
@@ -51,60 +60,65 @@ private fun toTsStruct(
 ): String {
     val optionalKeys = mutableListOf<String>()
     val objectResult = mutableListOf<String>()
-    val json =  jsonElement.asJsonObject
+    val json = jsonElement.asJsonObject
     val entrySet = json.entrySet();
 
     for (o in entrySet) {
         val value = o.value
         val key = o.key
-        if (value.isJsonObject && !value.isJsonArray) {
-            val childObjectName = uppercaseFirstChar(key)
-            val typeString = toTsStruct(
-                jsonElement = value,
-                parseType = parseType,
-                objectName = childObjectName,
-                structResult = structResult
-            )
-            if (!structResult.contains(typeString)) {
-                objectResult.add(typeString)
-                structResult.add(typeString)
-            }
-            json.addProperty(key, "$childObjectName;")
-        } else if (value.isJsonArray) {
-            val valueArr = value.asJsonArray
-            val arrayTypes = detectMultiArrayTypes(value)
-            if (arrayTypes.size > 1) {
-                val multiArrayBrackets = getMultiArrayBrackets(value.toString())
-                if (isAllEquals(arrayTypes)) {
-                    json.addProperty(key, arrayTypes[0].replace("[]", multiArrayBrackets))
-
-                } else {
-                    json.addProperty(key, "any$multiArrayBrackets;")
-                }
-
-            } else if (valueArr.size() > 0 && valueArr[0].isJsonObject) {
+        when {
+            value.isJsonObject && !value.isJsonArray -> {
                 val childObjectName = uppercaseFirstChar(key)
-                objectResult.add(toTsStruct(valueArr[0], parseType, childObjectName, structResult))
-                json.addProperty(key, removeMajority(childObjectName) + "[];")
-            } else {
-                json.addProperty(key, arrayTypes[0])
+                val typeString = toTsStruct(
+                    jsonElement = value,
+                    parseType = parseType,
+                    objectName = childObjectName,
+                    structResult = structResult
+                )
+                if (!structResult.contains(typeString)) {
+                    objectResult.add(typeString)
+                    structResult.add(typeString)
+                }
+                json.addProperty(key, "$childObjectName;")
             }
-        } else if (value.isJsonPrimitive) {
-            val jsonPrimitive = value.asJsonPrimitive;
-            when {
-                jsonPrimitive.isBoolean -> {
-                    json.addProperty(key, "boolean;");
-                }
-                jsonPrimitive.isString -> {
-                    json.addProperty(key, "string;");
-                }
-                jsonPrimitive.isNumber -> {
-                    json.addProperty(key, "number;");
+            value.isJsonArray -> {
+                val valueArr = value.asJsonArray
+                val arrayTypes = detectMultiArrayTypes(value)
+                if (arrayTypes.size > 1) {
+                    val multiArrayBrackets = getMultiArrayBrackets(value.toString())
+                    if (isAllEquals(arrayTypes)) {
+                        json.addProperty(key, arrayTypes[0].replace("[]", multiArrayBrackets))
+
+                    } else {
+                        json.addProperty(key, "any$multiArrayBrackets;")
+                    }
+
+                } else if (valueArr.size() > 0 && valueArr[0].isJsonObject) {
+                    val childObjectName = uppercaseFirstChar(key)
+                    objectResult.add(toTsStruct(valueArr[0], parseType, childObjectName, structResult))
+                    json.addProperty(key, removeMajority(childObjectName) + "[];")
+                } else {
+                    json.addProperty(key, arrayTypes[0])
                 }
             }
-        } else {
-            json.addProperty(key, "any;")
-            optionalKeys.add(key)
+            value.isJsonPrimitive -> {
+                val jsonPrimitive = value.asJsonPrimitive;
+                when {
+                    jsonPrimitive.isBoolean -> {
+                        json.addProperty(key, "boolean;");
+                    }
+                    jsonPrimitive.isString -> {
+                        json.addProperty(key, "string;");
+                    }
+                    jsonPrimitive.isNumber -> {
+                        json.addProperty(key, "number;");
+                    }
+                }
+            }
+            else -> {
+                json.addProperty(key, "any;")
+                optionalKeys.add(key)
+            }
         }
     }
     val result = formatCharsToTypeScript(json, objectName, optionalKeys, parseType)
@@ -155,21 +169,28 @@ private fun detectMultiArrayTypes(
     list.addAll(valueType)
     if (value.isJsonArray) {
         val arr = value.asJsonArray;
-        if (arr.size() == 0) {
-            list.add("any[];")
-        } else if (arr[0].isJsonArray) {
-            for (item in arr) {
-                val valueTypeResult = detectMultiArrayTypes(item, valueType)
-                list.addAll(valueTypeResult)
+        when {
+            arr.size() == 0 -> {
+                list.add("any[];")
             }
-        } else if (checkJsonArrayType(arr, ::checkIsString)) {
-            list.add("string[];")
-        } else if (checkJsonArrayType(arr, ::checkIsNumber)) {
-            list.add("number[];")
-        } else if (checkJsonArrayType(arr, ::checkIsBoolean)) {
-            list.add("boolean[]")
-        } else {
-            list.add("any[]")
+            arr[0].isJsonArray -> {
+                for (item in arr) {
+                    val valueTypeResult = detectMultiArrayTypes(item, valueType)
+                    list.addAll(valueTypeResult)
+                }
+            }
+            checkJsonArrayType(arr, ::checkIsString) -> {
+                list.add("string[];")
+            }
+            checkJsonArrayType(arr, ::checkIsNumber) -> {
+                list.add("number[];")
+            }
+            checkJsonArrayType(arr, ::checkIsBoolean) -> {
+                list.add("boolean[]")
+            }
+            else -> {
+                list.add("any[]")
+            }
         }
 
     }
