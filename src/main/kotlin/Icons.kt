acc809a178d578a804a93ca13bcec6a26471b8a6
vidy <vidy3587@gmail.com>
2020-07-14 10:16:39 +0800

    add action icon

---
+++ src/main/kotlin/Icons.kt
@@ -0,0 +1,15 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object Icons {
+
+    @JvmField
+    val pluginIcon: Icon = IconLoader.getIcon("/icons/pluginIcon.svg").apply {
+//        set
+    }
+
+
+
+}
